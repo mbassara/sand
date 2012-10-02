bcacf151848a38b1a44ad65327a54a9b95dbeb69
Maciek <mbassara@gmail.com>
2012-10-02 14:20:24 +0200

    existing files added to repository

---
+++ MainWindow.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Sand
+{
+    class MainWindow : Form
+    {
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Graphics img;
+
+        private bool MouseIsDown = false;
+        private Label label1;
+        private Label label2;
+        private ComboBox sizeComboBox;
+        private ComboBox speedComboBox;
+        private SandBox sandBox;
+
+        private int threadCount = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyInitializeComponent();
+
+            Debug.Listeners.Add(new TextWriterTraceListener(System.Console.Out));
+            img = Graphics.FromHwnd(pictureBox1.Handle);
+
+
+            sandBox = SandBox.GetInstance(img, Math.Min(pictureBox1.ClientSize.Height, pictureBox1.ClientSize.Width), 1, 30);
+
+            Thread thread = new Thread(new ThreadStart(delegate()
+            {
+                sandBox.Run(1000000);
+            }));
+            thread.Name = "Sand Thread " + threadCount++;
+            thread.Start();
+
+            FormClosing += delegate(object o, FormClosingEventArgs e) { sandBox.IsRunning = false; };
+
+            pictureBox1.MouseDown += delegate(object o, MouseEventArgs evt)
+            {
+                MouseIsDown = true;
+                sandBox.FallingPoint = evt.Location;
+                sandBox.IsPaused = false;
+            };
+            pictureBox1.MouseUp += delegate(object o, MouseEventArgs evt)
+            {
+                MouseIsDown = false;
+                sandBox.FallingPoint = evt.Location;
+                sandBox.IsPaused = true;
+            };
+            pictureBox1.MouseMove += delegate(object o, MouseEventArgs evt)
+            {
+                if(MouseIsDown)
+                    sandBox.FallingPoint = evt.Location;
+            };
+
+        }
+
+        private void MyInitializeComponent()
+        {
+            this.sizeComboBox.Items.AddRange(new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            this.sizeComboBox.SelectedIndex = 0;
+            this.sizeComboBox.SelectedValueChanged += size_changed;
+
+            this.speedComboBox.Items.AddRange(new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            this.speedComboBox.SelectedIndex = 7;
+            this.speedComboBox.SelectedValueChanged += speed_changed;
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sizeComboBox = new System.Windows.Forms.ComboBox();
+            this.speedComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(338, 338);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(410, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grain size:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Game speed:";
+            // 
+            // sizeComboBox
+            // 
+            this.sizeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sizeComboBox.FormattingEnabled = true;
+            this.sizeComboBox.Location = new System.Drawing.Point(466, 117);
+            this.sizeComboBox.Name = "sizeComboBox";
+            this.sizeComboBox.Size = new System.Drawing.Size(52, 21);
+            this.sizeComboBox.TabIndex = 4;
+            // 
+            // speedComboBox
+            // 
+            this.speedComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.speedComboBox.FormattingEnabled = true;
+            this.speedComboBox.Location = new System.Drawing.Point(466, 145);
+            this.speedComboBox.Name = "speedComboBox";
+            this.speedComboBox.Size = new System.Drawing.Size(51, 21);
+            this.speedComboBox.TabIndex = 5;
+            // 
+            // MainWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(554, 363);
+            this.Controls.Add(this.speedComboBox);
+            this.Controls.Add(this.sizeComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        public static void Main()
+        {
+            Application.Run(new MainWindow());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sandBox.Reset();
+            Thread thread = new Thread(new ThreadStart(delegate()
+            {
+                sandBox.Run(1000000);
+            }));
+            thread.Name = "Sand Thread " + threadCount++;
+            thread.Start();
+        }
+
+        private void size_changed(object sender, EventArgs e)
+        {
+            int delay = 10 - speedComboBox.SelectedIndex - 1;
+            int pixelSize = sizeComboBox.SelectedIndex + 1;
+            sandBox.Reset(Math.Min(pictureBox1.ClientSize.Height, pictureBox1.ClientSize.Width), pixelSize, delay);
+            
+            Thread thread = new Thread(new ThreadStart(delegate()
+            {
+                sandBox.Run(1000000);
+            }));
+            thread.Name = "Sand Thread " + threadCount++;
+            thread.Start();
+        }
+
+        private void speed_changed(object sender, EventArgs e)
+        {
+            int delay = 10 - speedComboBox.SelectedIndex - 1;
+            sandBox.Delay = delay * 10;
+        }
+
+    }
+}
---
+++ SandBox.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+
+namespace Sand
+{
+    class SandBox
+    {
+        private int size;
+        private int pixelSize;
+
+        private bool[,] box;
+        private Graphics img;
+
+        private Point fallingPoint;
+        public Point FallingPoint
+        {
+            get { return fallingPoint; }
+            set
+            {
+                if (value.X / pixelSize < size && value.Y / pixelSize < size &&
+                    value.X / pixelSize >= 0 && value.Y / pixelSize >= 0)
+                    fallingPoint = new Point(value.X / pixelSize, value.Y / pixelSize);
+            }
+        }
+
+        private bool isRunning;
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set { isRunning = value; }
+        }
+
+        private bool isPaused;
+        public bool IsPaused
+        {
+            get { return isPaused; }
+            set { isPaused = value; }
+        }
+
+        private int delay;
+        public int Delay
+        {
+            get { return delay; }
+            set { delay = value; }
+        }
+
+        private static Dictionary<Graphics, SandBox> instances = new System.Collections.Generic.Dictionary<Graphics,SandBox>();
+
+        public static SandBox GetInstance(Graphics img, int size, int pixelSize, int delay)
+        {
+            if (!instances.ContainsKey(img))
+                instances.Add(img, new SandBox(img, size, pixelSize, delay));
+
+            return instances[img];
+        }
+
+        private SandBox(Graphics img, int size, int pixelSize, int delay)
+        {
+            this.size = size / pixelSize;
+            this.pixelSize = pixelSize;
+            this.img = img;
+            this.img.Clear(Color.White);
+            this.delay = delay;
+
+            isRunning = false;
+            isPaused = true;
+            fallingPoint = new Point(size / 2, 0);
+            box = new bool[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    box[i, j] = false;
+
+            for (int i = 0; i < size; i++)
+                box[size-1, i] = true;
+        }
+
+        public void Reset()
+        {
+            Reset(size * pixelSize /*size in pixels*/, pixelSize, delay);
+        }
+
+        public void Reset(int size, int pixelSize, int delay)
+        {
+            isRunning = false;
+            isPaused = true;
+
+            this.size = size / pixelSize;
+            this.pixelSize = pixelSize;
+            this.img.Clear(Color.White);
+
+            fallingPoint = new Point(size / 2, 0);
+            box = new bool[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    box[i, j] = false;
+
+            for (int i = 0; i < size; i++)
+                box[size - 1, i] = true;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void Run(int iterations)
+        {
+            System.Diagnostics.Debug.WriteLine("Run start, Thread: " + System.Threading.Thread.CurrentThread.Name);
+            if (isRunning)
+                return;
+
+            img.Clear(Color.White);
+
+            isRunning = true;
+            for (int i = 0; i < iterations && isRunning; i++)
+            {
+                Iterate();
+
+                if(delay > 0)
+                    System.Threading.Thread.Sleep(delay);
+            }
+            isRunning = false;
+            System.Diagnostics.Debug.WriteLine("Run end, Thread: " + System.Threading.Thread.CurrentThread.Name);
+        }
+
+        private void MakeGrain(int x, int y)
+        {
+            box[x, y] = true;
+        }
+
+
+        private void Iterate()
+        {
+            if (!isPaused)
+                MakeGrain(fallingPoint.Y, fallingPoint.X);
+            bool[,] before = DuplicateBox();
+
+            for (int i = 0; i + 1 < size && isRunning; i += 2)
+                for (int j = 0; j + 1 < size && isRunning; j += 2)
+                    NextState(ref box[i, j], ref box[i, j + 1], ref box[i + 1, j], ref box[i + 1, j + 1]);
+
+            for (int i = 1; i + 1 < size && isRunning; i += 2)
+                for (int j = 1; j + 1 < size && isRunning; j += 2)
+                    NextState(ref box[i, j], ref box[i, j + 1], ref box[i + 1, j], ref box[i + 1, j + 1]);
+
+            for (int i = 0; i + 1 < size && isRunning; i++)
+                for (int j = 0; j + 1 < size && isRunning; j++)
+                    if (box[i, j] != before[i, j] && isRunning)
+                    {
+                        if (box[i, j])
+                            img.FillRectangle(Brushes.Black, j * pixelSize, i * pixelSize, pixelSize, pixelSize);
+                        else
+                            img.FillRectangle(Brushes.White, j * pixelSize, i * pixelSize, pixelSize, pixelSize);
+                    }
+        }
+
+        private bool[,] DuplicateBox()
+        {
+            bool[,] newBox = new bool[size, size];
+
+            for (int i = 0; i < size && isRunning; i++)
+                for (int j = 0; j < size && isRunning; j++)
+                    newBox[i, j] = box[i, j];
+            return newBox;
+        }
+
+        private void NextState(ref bool ul,
+                                ref bool ur,
+                                ref bool bl,
+                                ref bool br)
+        {
+            if (!ul && !ur && !bl && !br)
+                return; // empty
+
+            bool t = true, f = false;
+
+            if (ul && !ur &&
+                !bl && !br)
+            { // 1
+                ul = f; ur = f;
+                bl = t; br = f;
+            }
+            else if (!ul && ur &&
+                     !bl && !br)
+            { // 2
+                ul = f; ur = f;
+                bl = f; br = t;
+            }
+            else if (ul && !ur &&
+                     bl && !br)
+            { // 3
+                ul = f; ur = f;
+                bl = t; br = t;
+            }
+            else if (!ul && ur &&
+                     !bl && br)
+            { // 4
+                ul = f; ur = f;
+                bl = t; br = t;
+            }
+            else if (ul && ur &&
+                     bl && !br)
+            { // 5
+                ul = t; ur = f;
+                bl = t; br = t;
+            }
+            else if (ul && ur &&
+                     !bl && br)
+            { // 6
+                ul = f; ur = t;
+                bl = t; br = t;
+            }
+            else if (!ul && ur &&
+                     bl && !br)
+            { // 7
+                ul = f; ur = f;
+                bl = t; br = t;
+            }
+            else if (ul && !ur &&
+                     !bl && br)
+            { // 8
+                ul = f; ur = f;
+                bl = t; br = t;
+            }
+            else if (ul && ur &&
+                     !bl && !br)
+            { // 9 block
+                if (new Random().Next() % 2 == 0)
+                {
+                    ul = f; ur = f;
+                    bl = t; br = t;
+                }
+                else
+                {
+                    ul = t; ur = t;
+                    bl = f; br = f;
+                }
+            }
+        }
+    }
+}
